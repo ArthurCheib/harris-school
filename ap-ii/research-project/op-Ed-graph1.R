b974bb24b82e91765b202bb7440c760328a370a6
Arthur Cheib <arthursilvacheib@gmail.com>
2022-02-20 12:27:12 -0600

    First graph code finished

--- ap-ii/research-project/op-Ed-graph1.R
+++ ap-ii/research-project/op-Ed-graph1.R
@@ -8,30 +8,12 @@ url_file <- "https://raw.githubusercontent.com/ArthurCheib/harris-school/main/ap
 dataset <- read.csv(url_file) %>% 
   as_tibble()
 
-###
+### Getting the values of the boxplot
 division <- summary(dataset$TotalEscolas)
 
 division[[2]]
 division[[3]]
 division[[5]]
-
-## Making graph 1: investment X political parties
-dataset %>% 
-  select(Ano, CODMUN, partido, invest, investAluno, IDEB, TotalEscolas) %>% 
-  filter(partido != 0) %>% 
-  mutate(amount_schools = case_when(TotalEscolas <= division[[2]] ~ "2 ou menos",
-                                    TotalEscolas > division[[2]] & TotalEscolas <= division[[3]] ~ "Entre 3 e 6",
-                                    TotalEscolas > division[[3]] & TotalEscolas <= division[[5]] ~ "Entre 6 e 15",
-                                             TRUE ~ "Acima de 15")) %>% 
-  filter(investAluno <= 60000 & investAluno > 1200 & Ano == 2018) %>% 
-  filter(!amount_schools %in% c("2 ou menos")) %>%
-  ggplot(aes(x = partido, y = investAluno)) +
-  geom_point() +
-  #facet_wrap(~amount_schools) +
-  geom_smooth(method = "lm") +
-  theme_light() +
-  scale_x_continuous(breaks = c(1,2,3), labels = c("Right-wing", "Centrist", "Left-wing"))
-  
 
 ## Making graph 1 (second attempt): investment X political parties
 colnames(dataset)[34] <- "sec_exclusiva"
@@ -61,9 +43,11 @@ dataset %>%
        y = "",
        fill = "",
        x = "",
-       caption = "Source: Brazilian Federal Government") +
+       caption = "Source: Brazilian Government - TCE | MUNIC | Ministry of Education") +
   theme(legend.position = "none",
-        plot.title = element_text(size = 14.5),
-        plot.subtitle = element_text(size = 12),
+        plot.title = element_text(size = 16.5),
+        plot.subtitle = element_text(size = 13.5),
         plot.caption = element_text(size = 12))
+
+
 
