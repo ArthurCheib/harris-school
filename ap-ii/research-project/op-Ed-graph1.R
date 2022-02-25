3570bad070bf7c5ed4b81bdcc787e8a9fca37a86
Arthur Cheib <arthursilvacheib@gmail.com>
2022-02-25 15:26:50 -0600

    Op-ed graphs

--- ap-ii/research-project/op-Ed-graph1.R
+++ ap-ii/research-project/op-Ed-graph1.R
@@ -50,4 +50,19 @@ dataset %>%
         plot.caption = element_text(size = 12))
 
 
-
+## Data
+dataset %>% 
+  select(Ano, CODMUN, partido, invest, investAluno, IDEB, TotalEscolas, sec_exclusiva) %>% 
+  filter(partido != 0) %>% 
+  mutate(amount_schools = case_when(TotalEscolas <= division[[2]] ~ "2 ou menos",
+                                    TotalEscolas > division[[2]] & TotalEscolas <= division[[3]] ~ "Entre 3 e 6",
+                                    TotalEscolas > division[[3]] & TotalEscolas <= division[[5]] ~ "Entre 6 e 15",
+                                    TRUE ~ "Acima de 15")) %>% 
+  filter(investAluno <= 60000 & investAluno > 1200 & Ano == 2018) %>% 
+  filter(!amount_schools %in% c("2 ou menos")) %>%
+  filter(sec_exclusiva != 0) %>% 
+  mutate(partido = factor(case_when(partido == 1 ~ "Right-wing",
+                                    partido == 2 ~ "Centrist",
+                                    partido == 3 ~ "Left-wing"), levels = c("Left-wing", "Centrist", "Right-wing"))) %>% 
+  group_by(partido) %>% 
+  summarize(avg_investment = mean(investAluno))
--- ap-ii/research-project/op-Ed-graph2.R
+++ ap-ii/research-project/op-Ed-graph2.R
@@ -19,15 +19,39 @@ division[[5]]
 colnames(dataset)[34] <- "sec_exclusiva"
 
 dataset_graph <- dataset %>% 
-  select(Ano, CODMUN, partido, invest, investAluno, IDEB, TotalEscolas, sec_exclusiva) %>% 
+  select(Ano, CODMUN, partido, invest, investAluno, IDEB, TotalEscolas, sec_exclusiva, PME) %>% 
   filter(partido != 0) %>% 
   mutate(amount_schools = case_when(TotalEscolas <= division[[2]] ~ "2 ou menos",
                                     TotalEscolas > division[[2]] & TotalEscolas <= division[[3]] ~ "Entre 3 e 6",
                                     TotalEscolas > division[[3]] & TotalEscolas <= division[[5]] ~ "Entre 6 e 15",
                                     TRUE ~ "Acima de 15")) %>% 
-  filter(investAluno <= 60000 & investAluno > 1200 & Ano == 2018) %>% 
+  filter(investAluno <= 60000 & investAluno > 1200) %>% 
   filter(!amount_schools %in% c("2 ou menos")) %>%
   filter(sec_exclusiva != 0)
 
 ## Making the 2nd graph - IDEB x PME
-dataset_graph
+dataset_graph %>% 
+  group_by(Ano, PME) %>% 
+  summarize(avg_ideb = round(mean(IDEB), digits = 2)) %>% 
+  ungroup() %>% 
+  mutate(PME = factor(case_when(PME == 1 ~ "Adopted PME",
+                         TRUE ~ "Hasn't adopted PME"),levels = c("Hasn't adopted PME", "Adopted PME"))) %>% 
+  ggplot(aes(x = PME, y = avg_ideb, fill = PME)) +
+  geom_col() +
+  theme_wsj() +
+  facet_wrap(~Ano) +
+  #scale_fill_tol("light") +
+  scale_fill_manual(values = c("#9999CC", "#66CC99")) +
+  scale_y_continuous(limits = c(0,6), breaks = c(seq(from = 0, to = 6, by = 1))) +
+  labs(title ="IDEB variation according to the presence of PME",
+       subtitle = "Standardized tests scores difference between the level of management capacity",
+       y = "",
+       fill = "",
+       x = "",
+       caption = "Source: Brazilian Government - MUNIC | Ministry of Education") +
+  geom_text(aes(label = avg_ideb), vjust = -0.5, size = 5) +
+  theme(legend.position = "none",
+        plot.title = element_text(size = 16.5),
+        plot.subtitle = element_text(size = 13.5),
+        plot.caption = element_text(size = 12))
+  
